f7f6c654e825216ccd7bb8c7f628b9212dfd0ff2
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2019-11-02 08:21:18 +0900

    空のtableviewとcellを作成

---
+++ GithubMVVMFlux/Trend/TrendTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrendTableViewCell.swift
+//  GithubMVVMFlux
+//
+//  Created by 横山新 on 2019/11/02.
+//  Copyright © 2019 ARATAYOKOYAMA. All rights reserved.
+//
+
+import UIKit
+
+final class TrendTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
