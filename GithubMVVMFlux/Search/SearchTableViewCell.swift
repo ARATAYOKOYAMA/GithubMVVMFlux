1db4a4a671fffd2b5e60105781e0166745774ac2
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2019-11-12 23:39:20 +0900

    xibとclassをつなげた

--- GithubMVVMFlux/Search/SearchTableViewCell.swift
+++ GithubMVVMFlux/Search/SearchTableViewCell.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
-class SearchTableViewCell: UITableViewCell {
+final class SearchTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var backView: UIView! {
+        didSet {
+            backView.backgroundColor = UIColor.white
+            backView.layer.borderColor = UIColor.black.cgColor
+            backView.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet private weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.numberOfLines = 0
+            titleLabel.font = titleLabel.font.withSize(20)
+            titleLabel.textColor = UIColor.blue
+        }
+    }
+    @IBOutlet private weak var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.numberOfLines = 0
+            descriptionLabel.font = descriptionLabel.font.withSize(16)
+            descriptionLabel.textColor = UIColor.gray
+        }
+    }
+    @IBOutlet private weak var languageLabel: UILabel! {
+        didSet {
+            languageLabel.font = languageLabel.font.withSize(12)
+            languageLabel.textColor = UIColor.gray
+        }
+    }
+    @IBOutlet private weak var staraLabel: UILabel! {
+        didSet {
+            staraLabel.font = staraLabel.font.withSize(12)
+            staraLabel.textColor = UIColor.gray
+        }
+    }
+    @IBOutlet private weak var updatedLabel: UILabel! {
+        didSet {
+            updatedLabel.font = updatedLabel.font.withSize(12)
+            updatedLabel.textColor = UIColor.gray
+        }
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
