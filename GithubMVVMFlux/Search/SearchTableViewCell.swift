f3b5b9911c79f1b0715a5803614fab5d650f27d3
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2019-11-12 23:18:24 +0900

    検索結果を描画するcellのxibを作成した

---
+++ GithubMVVMFlux/Search/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  GithubMVVMFlux
+//
+//  Created by 横山新 on 2019/11/12.
+//  Copyright © 2019 ARATAYOKOYAMA. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
