d81446128977f121cfedf9f386b2a8b6a8d40bdd
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2019-12-31 23:16:00 +0900

    run swiftgen

--- GithubMVVMFlux/Sources/Text.swift
+++ GithubMVVMFlux/Sources/Text.swift
@@ -12,33 +12,33 @@ import Foundation
 // swiftlint:disable nesting type_body_length type_name
 internal enum L10n {
 
-    internal enum Navigation {
-        internal enum Search {
-            /// Search
-            internal static let title = L10n.tr("Text", "navigation.search.title")
-        }
-        internal enum Trend {
-            /// Trending
-            internal static let title = L10n.tr("Text", "navigation.trend.title")
-        }
-        internal enum UserFavorate {
-            /// Your Favorate
-            internal static let title = L10n.tr("Text", "navigation.user_favorate.title")
-        }
-        internal enum UserRepositories {
-            /// Your Repositories
-            internal static let title = L10n.tr("Text", "navigation.user_repositories.title")
-        }
+  internal enum Navigation {
+    internal enum Search {
+      /// Search
+      internal static let title = L10n.tr("Text", "navigation.search.title")
     }
+    internal enum Trend {
+      /// Trending
+      internal static let title = L10n.tr("Text", "navigation.trend.title")
+    }
+    internal enum UserFavorate {
+      /// Your Favorate
+      internal static let title = L10n.tr("Text", "navigation.user_favorate.title")
+    }
+    internal enum UserRepositories {
+      /// Your Repositories
+      internal static let title = L10n.tr("Text", "navigation.user_repositories.title")
+    }
+  }
 
-    internal enum TableCell {
-        internal enum Trend {
-            /// %@ / %@
-            internal static func title(_ p1: String, _ p2: String) -> String {
-                return L10n.tr("Text", "table_cell.trend.title", p1, p2)
-            }
-        }
+  internal enum TableCell {
+    internal enum Trend {
+      /// %@ / %@
+      internal static func title(_ p1: String, _ p2: String) -> String {
+        return L10n.tr("Text", "table_cell.trend.title", p1, p2)
+      }
     }
+  }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name
@@ -46,11 +46,11 @@ internal enum L10n {
 // MARK: - Implementation Details
 
 extension L10n {
-    private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
-        // swiftlint:disable:next nslocalizedstring_key
-        let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
-        return String(format: format, locale: Locale.current, arguments: args)
-    }
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    // swiftlint:disable:next nslocalizedstring_key
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
 }
 
 private final class BundleToken {}
